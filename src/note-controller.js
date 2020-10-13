ccbca15e5a540bed3c28af890b5b9ee10ebfc786
Johnny Yip <jyippy2012@gmail.com>
2020-10-13 15:39:14 +0100

    Spiking note-controller

--- src/note-controller.js
+++ src/note-controller.js
@@ -3,7 +3,15 @@
 class NoteController {
 
   constructor (noteList){
-    this.noteList = new NoteList(noteList);
+    this.noteList = noteList;
+    this.noteList.create("Favourite drink: seltzer")
+    this.noteListView = new NoteListView(noteList)
+  }
+
+  insertIntoHTML(){
+      var html = this.noteListView.outputToHtml()
+      var element = document.getElementById("app")
+      element.innerHTML = html
   }
 
 }
